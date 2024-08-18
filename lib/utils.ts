54f931864082f6308b58c3d34037781a291d40d8
AgustinSegui <agussegui24@hotmail.es>
2024-08-18 20:11:24 -0300

    strip.ts configurate from pay and version. utils.ts configurate bargaining chip

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const formatter = new Intl.NumberFormat("en-AR", {
+export const formatter = new Intl.NumberFormat("en-US", {
   style: 'currency',
-  currency: 'ARS'
+  currency: 'USD'
 })
